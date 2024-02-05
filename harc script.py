fa7e079b7a2a6eb7a397bc2ea7cb5a62dbc83443
Fortimiklos <153606269+Fortimiklos@users.noreply.github.com>
2024-02-05 22:04:29 +0100

    egy kis hotfix de játszani egymagában is lehet
    holnap jön az update ikt-n

--- harc script.py
+++ harc script.py
@@ -1,34 +1,45 @@
 import random
 import os
+import time
 dmg=1
 élet=20
 erő=20
 luck=random.randint(1,20)
 es=1
-kut=1
 fight=1
 re=40
-if es==1:
-    print("Valami megtámadott")
-    while fight!=0:
-        print("                                     életereje:"+str(re))
-        dmge=random.randint(1,élet-5)
-        print("sebzett rád "+str(dmge)+"-t")
-        élet=élet-dmge
+realhp=élet-7
+valami=1
+while fight!=0:
+    if valami==1:
+        print("Valami megtámadott")
+    valami=0
+    print("                                     életereje:"+str(re))
+    os.system('color 2')
+    dmge=random.randint(1,realhp)
+    print("Sebzett rád "+str(dmge)+"-t")
+    élet=élet-dmge
+    if élet<1:
+        os.system('cls')
+        kut=0
+        print("             Meghaltál")
+        print("Ne feledd az óceán veszélyesebb, mint hinnéd!")
+        os.system('color 4')
+        élet=realhp+5
+        re=40
+        valami=1
+        time.sleep(2)
+        os.system('cls')
+    elif élet>1:
         print("Támadj                               életerőd:"+str(élet))
-        print("(Támadás!)")
+        print("(Támadás)")
         t=input()
         os.system('cls')
-        if élet<1:
-            print("Meghaltál!")
-            kut=0
-            print("             Meghaltál")
-            print("Ne feledd az óceán veszélyesebb, mint hinnéd!")
-            os.system('color 4')
-        if t=="Támadás" and kut==1:
+        if t=="Támadás" or t=="támadás":
             seb=random.randint(1,erő*dmg)
             print("Sebeztél "+str(seb)+"-t")
             re=re-seb
+            os.system('color 2')
             if re<1:
                 print("Nyertél!")
                 fight=0
